241b29941bcccd96f873cd70100aec461f3a38ec
taey16 <taey1600@gmail.com>
2015-11-06 08:53:09 +0900

    moidfy parallel_utils primary_gpu_id

--- utils/parallel_utils.lua
+++ utils/parallel_utils.lua
@@ -2,18 +2,21 @@
 require 'cutorch'
 require 'cunn'
 
-function makeDataParallel(model, nGPU)
+function makeDataParallel(model, nGPU, primary_gpu_id)
   if nGPU > 1 then
     print('converting module to nn.DataParallelTable')
     assert(nGPU <= cutorch.getDeviceCount(), 
       'number of GPUs less than nGPU specified')
     local model_single = model
+    -- Split along first (batch) dimension
+    -- in our case it is batch_size
     model = nn.DataParallelTable(1)
-    for i=1, opt.nGPU do
+    for i=1, nGPU do
       cutorch.setDevice(i)
       model:add(model_single:clone():cuda(), i)
     end
-    --cutorch.setDevice(opt.GPU)
+    -- set 'primary' GPU
+    cutorch.setDevice(primary_gpu_id)
   end
   return model
 end
