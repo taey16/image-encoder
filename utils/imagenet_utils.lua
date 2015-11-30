5483aa22887cceef337a688f81531959d475862f
taey16 <taey1600@gmail.com>
2015-11-30 19:32:00 +0900

    modify imagenet_utils.lua

--- utils/imagenet_utils.lua
+++ utils/imagenet_utils.lua
@@ -16,13 +16,16 @@ function get_val()
   local file = io.open '/storage/ImageNet/ILSVRC2012/val_synset.txt'
   local image_list ={}
   local label_list ={}
+  local synset_list={}
 
   while true do
     local line = file:read()
     if not line then break end
-    item = string.split(line, ' ')
+    local item  =string.split(line, ' ')
+    local synset=string.split(item[1], '/')
     table.insert(image_list, item[1])
     table.insert(label_list, item[2])
+    table.insert(synset_list,synset[1])
   end
-  return image_list, label_list
+  return image_list, label_list, synset_list
 end
