87b78a3e25169ab06b3224c92e0cc7e9e1f29e3d
taey16 <taey1600@gmail.com>
2015-12-03 15:27:26 +0900

    setDevice in main.lua

--- main.lua
+++ main.lua
@@ -10,14 +10,14 @@ require 'nn'
 --local opts = paths.dofile('opts/opts_ilsvrc.lua')
 local opts = paths.dofile('opts/opts_det.lua')
 opt = opts.parse(arg)
-paths.dofile('model_stn.lua')
 
 --torch.setnumthreads(4)
 torch.setdefaulttensortype('torch.FloatTensor')
 torch.manualSeed(opt.manualSeed)
 cutorch.manualSeed(opt.manualSeed+4322)
---cutorch.setDevice(opt.GPU)
+cutorch.setDevice(opt.GPU)
 
+paths.dofile('model_stn.lua')
 paths.dofile('data.lua')
 paths.dofile('train.lua')
 paths.dofile('test.lua')
