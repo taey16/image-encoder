19a2ebfd7867fe7f4baaf93c36572eb4f82b4ed1
taey16 <taey1600@gmail.com>
2015-12-05 15:08:43 +0900

    bugfix parallel_utils.lua

--- utils/parallel_utils.lua
+++ utils/parallel_utils.lua
@@ -10,8 +10,9 @@ function makeDataParallel(model, nGPU, primary_gpu_id)
     -- Split along first (batch) dimension
     -- in our case it is batch_size
     model = nn.DataParallelTable(1)
-    for gpu_id in pairs(nGPU) do
+    for _,gpu_id in pairs(nGPU) do
       cutorch.setDevice(gpu_id)
+      print('DataParallelTable setDevice: '..gpu_id)
       model:add(model_single:clone():cuda(), gpu_id)
     end
     -- set 'primary' GPU
