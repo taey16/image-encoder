025d0e8a6d43dc624296acce3d5b6653efdf8212
taey16 <taey1600@gmail.com>
2015-12-23 09:30:52 +0900

    commant in models/init_model_weight.lua

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 # image-encoder
+
+model
+-- vgg16 from Caffe (with loadCaffe)
+-- inception5, inception6, inception7
+--- residual learning
--- models/init_model_weight.lua
+++ models/init_model_weight.lua
@@ -3,11 +3,19 @@ require 'nn'
 require 'cunn'
 require 'cudnn'
 
--- initialization from MSR
+-- Delving Deep into Rectifiers: 
+-- Suupassing Human-Level Performance on ImageNet Classification, 2014
+-- A sufficient condition is:
+-- \frac{1}{2} n_l \text{Var}[w_l] = 1, \forall l. (1)
+-- where n_l = k^2 * c
+-- (1) leads to a zero-mean Gaussian distribution whos std. is \sqrt{2/n_l}
+-- bias is zeros
 function MSRinit(net)
-  local function init(name)
-    for k, v in pairs(net:findModules(name)) do
+  local function init(module_type)
+    for k, v in pairs(net:findModules(module_type)) do
+      -- n = k^2 * c
       local n = v.kW * v.kH * v.nOutputPlane
+      -- \sqrt{2/n}
       v.weight:normal(0,math.sqrt(2/n))
       v.bias:zero()
     end
