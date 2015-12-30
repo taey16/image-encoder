f46fe0609bb2abd5a62fda9f7939dcbbd8987ed3
taey16 <taey1600@gmail.com>
2015-12-30 16:03:42 +0900

    bugfix remove local int local function sanitize.sanitize

--- utils/sanitize.lua
+++ utils/sanitize.lua
@@ -72,7 +72,7 @@ end
 
 -- Taken and modified from Soumith's imagenet-multiGPU.torch code
 -- https://github.com/soumith/imagenet-multiGPU.torch/blob/master/train.lua
-local function sanitize.sanitize(model)
+function sanitize.sanitize(model)
    local list = model:listModules()
    for _,val in ipairs(list) do
     for name,field in pairs(val) do
