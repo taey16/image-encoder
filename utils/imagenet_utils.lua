97698aa7535606245c9ed97b742472d74294b628
taey16 <taey1600@gmail.com>
2016-01-06 17:46:36 +0900

    add get_test()

--- utils/imagenet_utils.lua
+++ utils/imagenet_utils.lua
@@ -27,5 +27,29 @@ function get_val()
     table.insert(label_list, item[2])
     table.insert(synset_list,synset[1])
   end
+  file:close()
   return image_list, label_list, synset_list
 end
+
+
+function get_test()
+  local file = io.open('/storage/ImageNet/ILSVRC2012/test.txt', 'r')
+  local image_list = {}
+  local label_list = {}
+  local list_table = {}
+  while true do
+    local line = file:read()
+    if not line then break end
+    local item = string.split(line, ' ')
+    table.insert(image_list, item[1])
+    table.insert(label_list, item[2])
+    list_table[item[1]] = 
+      string.format(
+        'http://10.202.4.219:2596/PBrain/ImageNet/ILSVRC2012/test/%s', 
+      item[1])
+  end
+  file:close()
+  return image_list, label_list, list_table
+end
+
+
