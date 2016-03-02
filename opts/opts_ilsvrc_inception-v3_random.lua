7bacfc65ac8afe881e6d8ce512b43395f48a0dea
taey16 <taey1600@gmail.com>
2016-03-02 10:21:47 +0900

    commit

--- opts/opts_ilsvrc_inception-v3_random.lua
+++ opts/opts_ilsvrc_inception-v3_random.lua
@@ -13,7 +13,7 @@ function M.parse(arg)
     'resception'
   local loadSize  = {3, 342, 342}
   local sampleSize= {3, 299, 299}
-  local nGPU = {1}
+  local nGPU = {1,2}
   local current_epoch = 4
   local test_initialization = false
   local nClasses = 1000
