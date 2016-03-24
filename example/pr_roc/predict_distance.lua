b42c8a2ca08d58e75920378eadf9bd83d8c57d00
taey16 <taey1600@gmail.com>
2016-03-24 14:28:57 +0900

    add fashion pair

--- example/pr_roc/predict_distance.lua
+++ example/pr_roc/predict_distance.lua
@@ -4,41 +4,46 @@ require 'cutorch'
 require 'nn'
 require 'cunn'
 require 'cudnn'
+require 'optim'
 local Threads = require 'threads'
 package.path = '../../?.lua;'..package.path
 local parallel_utils = require 'utils.parallel_utils'
-local attribute_utils= require 'utils.attribute_utils'
+local fashion_pair_utils= require 'utils.fashion_pair_utils'
 
 
-local gpus = {1,2}
-local attribute_id = 
-  --'slit_collar'
-  'button'
-  --'china_collar'
-local model_filename = 
-  '/data2/ImageNet/ILSVRC2012/torch_cache/X_gpu1_resception_nag_lr0.00450_decay_start0_every160000/model_29.bn_removed.t7'
-print(string.format('===> Loading model: %s', model_filename))
+local gpus = {1}
+local metric = 'L2'
+local model_path = 
+  '/data2/ImageNet/ILSVRC2012/torch_cache/X_gpu1_resception_nag_lr0.00450_decay_start0_every160000/'
+local model_filename = paths.concat(model_path, 'model_29.bn_removed.t7')
+print(string.format( '===> Loading model: %s', model_filename))
 local model = torch.load(model_filename)
-model.remove(#model.modules)
-model.remove(#model.modules)
-model.remove(#model.modules)
-cudnn.fastest = true
-cudnn.benchmark = true
+model:remove(#model.modules)
+model:remove(#model.modules)
+if metric == 'L2' then
+  model:add(nn.Normalize(2))
+elseif metric == 'L1' then
+  model:add(nn.Normalize(1))
+end
+model:evaluate()
+cudnn.fastest = false
+cudnn.benchmark = false
 cudnn.verbose = false
 if #gpus > 1 then
   model = parallel_utils.makeDataParallel(model, gpus)
-else
-  model:cuda()
-end
+else model:cuda() end
 print(model)
 collectgarbage()
 
+local logger = optim.Logger(string.format(
+  '%s.distance.%s.log', model_filename, metric))
+
 print '===> Loading mean, std' 
 local mean_std = torch.load(
-  '/storage/freebee/attribute_button/torch_cache/meanstdCache.t7'
+  '/data2/ImageNet/ILSVRC2012/torch_cache/meanstdCache.t7'
 )
 
-local image_list, label_list = attribute_utils.get_test(attribute_id)
+local image_list_q, image_list_ref, label_list = fashion_pair_utils.get_test()
 
 local loadSize = {3, 342, 342}
 local sampleSize={3, 299, 299}
@@ -59,43 +64,52 @@ local donkeys = Threads(
   end
 )
 
-local top1 = 0
-local trials = 0
 
 local batch_timer= torch.Timer()
 local data_timer = torch.Timer()
-local testBatch = function(n, inputs, label)
+local testBatch = function(n, inputs_q, inputs_ref, label)
   local elapsed_loading = data_timer:time().real
   batch_timer:reset()
 
-  local score, scores, classes
-  local output = model.encoder:forward(inputs:cuda())
+  local output_q  = model:forward(inputs_q:cuda()):clone()
   cutorch.synchronize()
-  score = model.classifier:forward(output):float()
-  local elapsed_process = batch_timer:time().real
-  score = torch.mean(score,1):view(-1)
-  scores, classes = score:sort(true)
+  local output_ref= model:forward(inputs_ref:cuda())
+  cutorch.synchronize()
 
-  trials = trials + 1
-  if classes[1] == label+1 then top1 = top1 + 1 end
-  io.flush(print(
-    ("%d %s %d top1: %d/%d = %.5f, %.4f(%.3f)"):format(
-      n, score[2], label, top1 , trials, top1 / trials * 100,
-      elapsed_process, elapsed_loading )
-    )
-  )
+  output_q  = output_q:mean(1):squeeze()
+  output_ref= output_ref:mean(1):squeeze()
+
+  local distance
+  if metric == 'L2' then
+    distance = math.sqrt(torch.pow(output_q - output_ref, 2.0):sum())
+  elseif metric == 'L1' then
+    distance = torch.abs(output_q - output_ref):sum()
+  end
+  logger:add{
+    ['distance'] = distance,
+    ['label'] = label
+  }
+  print(string.format(
+    '%d %f %d %f %f', 
+    n, distance, label, batch_timer:time().real, elapsed_loading))
   data_timer:reset()
+
+  if n % 500 == 0 then collectgarbage() end
 end
 
 
 local global_timer = torch.Timer()
-for n, filename in ipairs(image_list) do
+for n, filename in ipairs(image_list_q) do
   donkeys:addjob(
     function()
+      local query_filename = filename
+      local ref_filename = image_list_ref[n]
       local label = label_list[n]
-      img = loader.inception7_aug20(
-        filename, loadSize, sampleSize, mean_std.mean, mean_std.std)
-      return n, img, label
+      local img_q  = loader.inception7_aug10(
+        query_filename, loadSize, sampleSize, mean_std.mean, mean_std.std)
+      local img_ref= loader.inception7_aug10(
+        ref_filename, loadSize, sampleSize, mean_std.mean, mean_std.std)
+      return n, img_q, img_ref, label
     end,
     testBatch
   )
--- utils/fashion_pair_utils.lua
+++ utils/fashion_pair_utils.lua
@@ -28,23 +28,25 @@ end
 function fashion_pair_utils.get_test()
   local filename = string.format(
     '/storage/product/fashion_pair/fashion_pair_test.csv')
-  local 
+  local prefix = '/data1/october_11st/october_11st_imgs/'
   local file = io.open(filename)
   if file == nil then
     error(string.format(
       'fashion_pair_utils.get_test, check filename: %s', filename))
   end
-  local image_list = {}
+  local image_list_q  = {}
+  local image_list_ref= {}
   local label_list = {}
   while true do
     local line = file:read()
     if not line then break end
     local item = string.split(line, ',')
-    table.insert(image_list, item[1])
-    table.insert(label_list, tonumber(item[2]))
+    table.insert(image_list_q,  string.format('%s/%s', prefix, item[1]))
+    table.insert(image_list_ref,string.format('%s/%s', prefix, item[2]))
+    table.insert(label_list, tonumber(item[3]))
   end
   file.close()
-  return image_list, label_list
+  return image_list_q, image_list_ref, label_list
 end
 
 return fashion_pair_utils
