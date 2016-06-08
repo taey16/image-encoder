7b7f295f0758fedc551d42c5f94ebaee62140f13
taey16 <taey1600@gmail.com>
2016-06-08 19:48:26 +0900

    add default opts

--- main.lua
+++ main.lua
@@ -3,15 +3,17 @@ require 'torch'
 require 'paths'
 
 opt = paths.dofile('opts/opts_ilsvrc-inception-v3-random.lua')
+--opt = paths.dofile('opts/opts_ilsvrc-resception-default.lua') -- default opts for resception
 
-paths.dofile('model.lua')
-paths.dofile('data.lua')
-paths.dofile('train.lua')
-paths.dofile('test.lua')
+paths.dofile('model.lua') -- creating model
+paths.dofile('data.lua') -- perpare ilsvrc2012 train/test cache
+paths.dofile('train.lua') -- include functions globally for training
+paths.dofile('test.lua') -- include functions globally for testing
 
 epoch = opt.epochNumber
-if opt.test_initialization then test() end
+if opt.test_initialization then test() end -- testing first if you need
 for i=1,opt.nEpochs do
+  -- main loop
   train()
   test()
   epoch = epoch + 1
